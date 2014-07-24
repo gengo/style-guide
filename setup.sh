bf184b28cf270ff5d3199e819dede1f66b3cf8df
vwxyz <feeddict@gmail.com>
2014-07-24 10:34:50 +0900

    update setup.sh, bower.json
    add "resolution" property  in "bower.json"
    
    - this is to escape dependency version conflict with "jquery".
    
    - jquery which is installed into "bower_components" is "1.7" though this jquery isnt used in "style-guide"
    
    - "style-guide" uses "jquery-1.9.0.js" which is downloaded from code.jquery.com
    
    change "bower install" related command in "setup.sh"
    
    - to simplify install process

--- setup.sh
+++ setup.sh
@@ -4,14 +4,4 @@ gem install compass
 gem install jekyll
 gem install scss-lint
 npm install
-bower install twbs/bootstrap-sass
-bower install https://github.com/twbs/bootstrap.git#gh-pages
-cd bower_components
-mv bootstrap bootstrap-docs
-cd ..
-bower install bootstrap-multiselect
-bower install x-editable
-bower install typeahead.js
-bower install select2
-bower install moment
-
+bower install bootstrap-docs=https://github.com/twbs/bootstrap.git#gh-pages
