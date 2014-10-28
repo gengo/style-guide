c61ce56377e567f2c2f626f6a2eeedd45df20820
Alexandre KIRILLOV <alexandre.kirillov@gmail.com>
2014-10-28 14:42:32 +0900

    deploy.sh: try mergeing master into gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
-echo "This is a pull request. No deployment will be done.";
-  exit 0;
-fi
+# if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
+# echo "This is a pull request. No deployment will be done.";
+#   exit 0;
+# fi
 
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-echo "This is not a deployable branch.";
-  exit 0;
-fi
+# if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+# echo "This is not a deployable branch.";
+#   exit 0;
+# fi
 
 echo "Set up $TRAVIS_REPO_SLUG [via travis] for $GIT_NAME <${GIT_EMAIL}>"
 export REPO_URL="https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
@@ -20,6 +20,14 @@ LAST_COMMIT=$(git log --oneline | head -n 1)
 
 # <new_branch> is created if it doesn’t exist; otherwise, it is reset.
 git checkout -B gh-pages
+
+echo "=== check gh-pages tree ==="
+git ls-tree --name-only gh-pages
+
+echo "=== marge master into gh-pages ==="
+git fetch $REPO_URL master
+git message $REPO_URL/master
+grunt dist
 
 echo "=== remove unnecessary files for deployment ==="
 rm -fr bower_components node_modules css fonts images js docs scss favicons dist
@@ -45,4 +53,4 @@ echo "=== git commit ==="
 echo "message :" $LAST_COMMIT
 git add -A .
 git commit -q -m "Travis build $TRAVIS_BUILD_NUMBER"
-git push -f $REPO_URL gh-pages 2> /dev/null
+git push -f $REPO_URL gh-pages
